46972682e08cef89c32493b95fd9355b97065349
geooo109 <sdi1400109@di.uoa.gr>
2019-04-03 06:06:23 +0300

    added auto generation

--- part1/src/ex4/main.py
+++ part1/src/ex4/main.py
@@ -1,15 +1,22 @@
 from point import *
 from plane import *
+import numpy as np
+import matplotlib.pyplot as plt
 def parse_input():
-    print("Give Points of the Plane in the form x,y to stop type ->quit<-:")
     points = []
-    while True:
-        in_str = raw_input()
-        #sometimes i forget what i have to write :P
-        if in_str == "quit" or in_str == "exit" or in_str == "stop":
-            break
-        in_str = in_str.split(",")
-        points.append(Point(int(in_str[0]),int(in_str[1])))
+    ch = raw_input("To autogenerate write ->AUTO<- else just type something: ")
+    if ch != "AUTO":
+        print("Give Points of the Plane in the form x,y to stop type ->quit<-: ")
+        while True:
+            in_str = raw_input()
+            #sometimes i forget what i have to write :P
+            if in_str == "quit" or in_str == "exit" or in_str == "stop":
+                break
+            in_str = in_str.split(",")
+            points.append(Point(int(in_str[0]),int(in_str[1])))
+    else:
+        num_points = int(raw_input("Enter number of Points: "))
+        points = np.array([Point(np.random.randint(0,400),np.random.randint(0,400)) for i in range(num_points)])
     return Plane(points)
 
 
