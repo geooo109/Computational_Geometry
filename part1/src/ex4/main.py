688be9a51c4634ff1dd8a364acbd23e497cdd8d8
geooo109 <sdi1400109@di.uoa.gr>
2019-04-03 03:00:27 +0300

    added things for gift wrap

---
+++ part1/src/ex4/main.py
@@ -0,0 +1,23 @@
+from point import *
+from plane import *
+def parse_input():
+  print("Give Points of the Plane in the form (x,y) to stop type ->quit<-:")
+  points = []
+  while True:
+    in_str = raw_input()
+    if in_str == "quit":
+      break
+    points.append(Point(int(in_str[0]),int(in_str[1])))
+  return Plane(points)
+
+
+def main():
+  try:
+    plane = parse_input()
+  except:
+    print("Error on input of the plane data points")
+    return
+  plane.print_plane()
+
+if __name__ == "__main__":
+  main()
